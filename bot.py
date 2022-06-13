a1048af8ff4b887fe765193c1b01ea0b40c17e74
aliexers <107118239+aliexers@users.noreply.github.com>
2022-06-13 16:22:47 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import glob
 from rubika.client import Bot
 import requests
 from rubika.tools import Tools
+from rubika import Bot
 from rubika.encryption import encryption
 from gtts import gTTS
 from mutagen.mp3 import MP3
@@ -12,6 +13,7 @@ import time
 import random
 import urllib
 import io
+from json import load , dump
 
 bot = Bot("pydroid 3", auth="wrychztzmhiqshmbkthytfudydggkwkm")
 target = "g0mj5C09dfcd676fa8a7c4e94c3edd58"
@@ -961,6 +963,70 @@ while True:
 								bot.sendMessage(target, responser,message_id=msg["message_id"])
 							except:
 								bot.sendMessage(target, "نتیجه رو پیویت فرستادم سید :)", message_id=msg["message_id"])
+								
+						elif msg["text"].startswith("یادبگیر") or msg["text"].startswith("/learn"):
+							try:
+								text = msg["text"].replace("یادبگیر ","").replace("/learn ","").split(":")
+								word = text[0]
+								answer = text[1]
+
+								data[word] = answer
+								with open("learn.json","w",encoding="utf-8") as learn:
+									dump(data, learn)
+
+								bot.sendMessage(target, "✅ ذخیره شد", msg["message_id"])
+							except:
+								bot.sendMessage(target, "❌ خطا در اجرای دستور", msg["message_id"])
+
+
+						elif msg["text"].startswith("افزودن ادمین") or msg["text"].startswith("/add_admin") :
+
+							try:
+								user = msg["text"].replace("افزودن ادمین ","").replace("/add_admin ","")[1:]
+								guid = bot.getInfoByUsername(user)["data"]["chat"]["abs_object"]["object_guid"]
+									
+								if not guid in admins :
+									bot.setGroupAdmin(target, guid)
+									bot.sendMessage(target, "✅ کاربر @"+ str(user) +" با موفقیت ادمین شد .", msg["message_id"])
+								else:
+									bot.sendMessage(target, "❌ کاربر هم اکنون ادمین میباشد", msg["message_id"])
+
+							except:
+							try:
+								guid = bot.getMessagesInfo(target, [msg["reply_to_message_id"]])[0]["author_object_guid"]
+								user = bot.getUserInfo(guid)["data"]["user"]["username"]
+										
+								if not guid in admins :
+									bot.setGroupAdmin(target, guid)
+									bot.sendMessage(target, "✅ کاربر @"+ str(user) +" با موفقیت ادمین شد .", msg["message_id"])
+								else:
+									bot.sendMessage(target, "❌ کاربر هم اکنون ادمین میباشد", msg["message_id"])
+							except:
+								bot.sendMessage(target, "❌ خطا در اجرای دستور", msg["message_id"])
+
+						elif msg["text"].startswith("حذف ادمین") or msg["text"].startswith("/del_admin") :
+							try:
+								user = msg["text"].replace("حذف ادمین ","").replace("/del_admin ","")[1:]
+								guid = bot.getInfoByUsername(user)["data"]["chat"]["abs_object"]["object_guid"]
+
+								if guid in admins :
+									bot.deleteGroupAdmin(target, guid)
+									bot.sendMessage(target, "✅ کاربر @"+ str(user) +" با موفقیت از ادمینی برکنار شد .", msg["message_id"])
+								else:
+									bot.sendMessage(target, "❌ کاربر ادمین گروه نمیباشد", msg["message_id"])
+
+							except:
+								try:
+									guid = bot.getMessagesInfo(target, [msg["reply_to_message_id"]])[0]["author_object_guid"]
+									user = bot.getUserInfo(guid)["data"]["user"]["username"]
+
+								if not guid in admins :
+										bot.setGroupAdmin(target, guid)
+										bot.sendMessage(target, "✅ کاربر @"+ str(user) +" با موفقیت از ادمینی برکنار شد .", msg["message_id"])
+								else:
+									bot.sendMessage(target, "❌ کاربر ادمین گروه نمیباشد", msg["message_id"])
+							except:
+								bot.sendMessage(target, "❌ خطا در اجرای دستور", msg["message_id"])
 								
 						elif "forwarded_from" in msg.keys() and bot.getMessagesInfo(target, [msg.get("message_id")])[0]["forwarded_from"]["type_from"] == "Channel" and not msg.get("author_object_guid") in admins :
 							try:
